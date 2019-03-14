91ed8dd4d9c763a08ff739dbe6e30e9911ab2eca
soren5 <pedrofilipecarvalho@mac.com>
2019-03-14 17:10:04 +0000

    fixed ipconfig-router

--- ipconfig-router.sh
+++ ipconfig-router.sh
@@ -1,3 +1,3 @@
-ifconfig eth1 23.214.219.254 netmask 255.255.255.0 up
-ifconfig eth2 192.168.10.254 netmask 255.255.255.0 up
-ifconfig eth3 87.248.214.97 netmask 255.255.255.0 up
+ifconfig enp0s8 23.214.219.254 netmask 255.255.255.0 up
+ifconfig enp0s9 192.168.10.254 netmask 255.255.255.0 up
+ifconfig enp0s10 87.248.214.97 netmask 255.255.255.0 up
